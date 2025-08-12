90b8d51319342e2c88b608ffa4f772f0b0b63b8d
David <davicolo717@gmail.com>
2025-08-12 09:08:15 -0400

    cosillas

--- draft.py
+++ draft.py
@@ -1,8 +1,42 @@
-from random import randint
+def menu():
+    print("Welcome to the Menu")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+print("Calculator")
 
-ans = randint(num1, num2)
+menu()
 
-print(f"The ramdom number is: {ans}")
+while True:
+    try:
+        opt = int(input("Enter your choice: "))
+        while opt != 5:
+            if opt == 1:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+                print(f"Result: {num1 + num2}")
+            elif opt == 2:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+                print(f"Result: {num1 - num2}")
+            elif opt == 3:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+                print(f"Result: {num1 * num2}")
+            elif opt == 4:
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+                try :
+                    print(f"Result: {num1 / num2}")
+                except ZeroDivisionError:
+                    print("Error: Division by zero is not allowed.")
+            elif opt == 5:
+                print("Exiting the calculator. Goodbye!")
+                break
+        else:
+            print("Invalid option, please try again.")
+    except ValueError:
+        print("Invalid input, please enter a number.")
